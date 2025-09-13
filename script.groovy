96ff23cab03b9a3515b89e67898d0b03d93dc554
eswar293 <eswarpkumar@gmail.com>
2025-09-13 13:13:44 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,16 +1,3 @@
-def buildjar() {
-    echo "Building the Application ..."
-    sh "mvn package"
-}
-
-def buildDockerImage() {
-echo "Building the docker iamge ..."
-    withCredentials([usernamePassword(credentialsId: 'Docker_Repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t eswar1241/my-repo:jma-1.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push eswar1241/my-repo:jma-1.0'
-    }
-}
 
 def deployApp() {
     echo "Deploying the application ..."
